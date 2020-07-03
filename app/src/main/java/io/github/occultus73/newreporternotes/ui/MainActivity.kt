ddff490b3eaecc4ff039d9b7f3d208de8e631958
John O. A. Rayner-Hilles <john.rayner-hilles@appexperts.me.uk>
2020-07-03 16:28:11 +0100

    fixed on activity result overrides nothing error.

--- app/src/main/java/io/github/occultus73/newreporternotes/ui/MainActivity.kt
+++ app/src/main/java/io/github/occultus73/newreporternotes/ui/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
         super.onActivityResult(requestCode, resultCode, data)
 
